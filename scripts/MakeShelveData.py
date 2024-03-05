fea838a4a7fa45c2501d0011a6293515c6f7a86d
LiChiehLin <llin148@ucr.edu>
2024-03-05 12:55:46 -0800

    Update MakeShelveData.py

--- scripts/MakeShelveData.py
+++ scripts/MakeShelveData.py
@@ -4,6 +4,7 @@ import glob
 import argparse
 import shelve
 import isce
+import isceobj
 from isceobj.Sensor import createSensor
 from isceobj.Util import Poly1D
 from isceobj.Planet.AstronomicalHandbook import Const
@@ -12,17 +13,19 @@ from isceobj.Planet.AstronomicalHandbook import Const
 # The output of this script should not be dependent on segments as the source script from ISCE does not take segment into acount in producing relevant files
 
 # Parse the required files to the script
-parser = argparse.ArgumentParser(description='Parse annotation, Doppler, segment and output directory')
+parser = argparse.ArgumentParser(description='Parse annotation, Doppler, segment, output directory, stitched SLC name')
 parser.add_argument('--annotation','-a',type=str,required=True)
 parser.add_argument('--doppler','-d',type=str,required=True)
 parser.add_argument('--segment','-s',type=str,required=True)
 parser.add_argument('--output','-o',type=str,required=True)
+parser.add_argument('--slc','-S',type=str,required=True)
 args = parser.parse_args()
 
 metaFile = args.annotation
 dopFile = args.doppler
 stackSegment = args.segment
 slcDir = args.output
+slcFile = args.slc
 
 # unpack (In unpackFrame_UAVSAR.py)
 # Corresponding codes of createSensor can be found in Github: isce2/components/isceobj/Sensor/UAVSAR_Stack.py
@@ -38,7 +41,25 @@ pickName = os.path.join(slcDir, 'data')
 with shelve.open(pickName) as db:
     db['frame'] = obj.frame
 
-print('Finished')
+# write_xml
+# "write_xml" is in isce2/contrib/stack/stripmapStack/prepareUAVSAR_coregStack.py
+# This is to produce xml and vrt files
+shelveFile = os.path.join(slcDir, 'data')
+with shelve.open(shelveFile,flag='r') as db:
+    frame = db['frame']
 
+length = frame.numberOfLines 
+width = frame.numberOfSamples
+print('')
+print(slcFile)
+print ('Width:',width,'Length:',length)
+
+slc = isceobj.createSlcImage()
+slc.setWidth(width)
+slc.setLength(length)
+slc.filename = slcFile
+slc.setAccessMode('write')
+slc.renderHdr()
+slc.renderVRT()
 
 
